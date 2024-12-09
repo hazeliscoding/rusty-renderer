b85384a917331eb8a04fb5d43f1c6e8fe97b6ed5
Hazel Granados <hazel.granados@bamtech.net>
2024-12-09 14:26:05 -0600

    🏷️ Add more vector math functions

--- src/vector.rs
+++ src/vector.rs
@@ -37,6 +37,25 @@ impl Vec2 {
         self.x * other.x + self.y * other.y
     }
 
+    /// Adds another vector to this vector.
+    ///
+    /// # Arguments
+    /// - `self`: The first `Vec2` to add to.
+    /// - `other`: The other `Vec2` to add to this vector.
+    ///
+    /// # Returns
+    /// A new `Vec2` representing the sum.
+    ///
+    /// # Formula
+    /// The sum of two vectors `a` and `b` is calculated as:
+    /// `a.x + b.x` and `a.y + b.y`.
+    pub fn add(&self, other: Vec2) -> Vec2 {
+        Vec2 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+
     /// Subtracts another vector from this vector.
     ///
     /// # Arguments
@@ -49,6 +68,59 @@ impl Vec2 {
             x: self.x - other.x,
             y: self.y - other.y,
         }
+    }
+
+    /// Multiplies this vector by a scalar value.
+    ///
+    /// # Arguments
+    /// - `self`: The `Vec2` to multiply.
+    /// - `scalar`: The scalar value to multiply this vector by.
+    ///
+    /// # Returns
+    /// A new `Vec2` representing the product.
+    ///
+    /// # Formula
+    /// The product of a vector `a` and a scalar `s` is calculated as:
+    /// `a.x * s` and `a.y * s`.
+    pub fn mul(&self, scalar: f32) -> Vec2 {
+        Vec2 {
+            x: self.x * scalar,
+            y: self.y * scalar,
+        }
+    }
+
+    /// Divides this vector by a scalar value.
+    ///
+    /// # Arguments
+    /// - `self`: The `Vec2` to divide.
+    /// - `scalar`: The scalar value to divide this vector by.
+    ///
+    /// # Returns
+    /// A new `Vec2` representing the quotient.
+    ///
+    /// # Formula
+    /// The quotient of a vector `a` and a scalar `s` is calculated as:
+    /// `a.x / s` and `a.y / s`.
+    pub fn div(&self, scalar: f32) -> Vec2 {
+        Vec2 {
+            x: self.x / scalar,
+            y: self.y / scalar,
+        }
+    }
+
+    /// Calculates the length of the vector.
+    ///
+    /// # Arguments
+    /// - `self`: The vector to calculate the length of.
+    ///
+    /// # Returns
+    /// The length of the vector as a `f32` value.
+    ///
+    /// # Formula
+    /// The length of a vector `a` is calculated as:
+    /// `sqrt(a.x^2 + a.y^2)`.
+    pub fn len(&self) -> f32 {
+        self.dot(*self).sqrt()
     }
 }
 
@@ -75,6 +147,112 @@ impl Vec3 {
     /// A `Vec3` instance with the given x, y, and z values.
     pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
         Vec3 { x, y, z }
+    }
+
+    /// Calculates the dot product of two vectors.
+    ///
+    /// # Arguments
+    /// - `self`: The first vector.
+    /// - `other`: The second vector.
+    ///
+    /// # Returns
+    /// The dot product as a `Vec3` value.
+    ///
+    /// # Formula
+    /// The dot product of two vectors `a` and `b` is calculated as:
+    /// `a x b = (a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x)`.
+    pub fn dot(&self, other: Vec3) -> f32 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    /// Multiplies this vector by a scalar value.
+    ///
+    /// # Arguments
+    /// - `self`: The vector to multiply.
+    /// - `scalar`: The scalar value to multiply this vector by.
+    ///
+    /// # Returns
+    /// A new `Vec3` representing the product.
+    ///
+    /// # Formula
+    /// The product of a vector `a` and a scalar `s` is calculated as:
+    /// `a.x * s`, `a.y * s`, and `a.z * s`.
+    pub fn mul(&self, scalar: f32) -> Vec3 {
+        Vec3 {
+            x: self.x * scalar,
+            y: self.y * scalar,
+            z: self.z * scalar,
+        }
+    }
+
+    /// Divides this vector by a scalar value.
+    /// 
+    /// # Arguments
+    /// - `self`: The vector to divide.
+    /// - `scalar`: The scalar value to divide this vector by.
+    /// 
+    /// # Returns
+    /// A new `Vec3` representing the quotient.
+    /// 
+    /// # Formula
+    /// The quotient of a vector `a` and a scalar `s` is calculated as:
+    /// `a.x / s`, `a.y / s`, and `a.z / s`.
+    pub fn div(&self, scalar: f32) -> Vec3 {
+        Vec3 {
+            x: self.x / scalar,
+            y: self.y / scalar,
+            z: self.z / scalar,
+        }
+    }
+
+    /// Calculates the length of the vector.
+    /// 
+    /// # Arguments
+    /// - `self`: The vector to calculate the length of.
+    /// 
+    /// # Returns
+    /// The length of the vector as a `f32` value.
+    /// 
+    /// # Formula
+    /// The length of a vector `a` is calculated as:
+    /// `sqrt(a.x^2 + a.y^2 + a.z^2)`.
+    pub fn len(&self) -> f32 {
+        self.dot(*self).sqrt()
+    }
+
+    /// Normalizes the vector.
+    /// 
+    /// # Arguments 
+    /// - `self`: The vector to normalize.
+    /// 
+    /// # Returns
+    /// A new `Vec3` representing the normalized vector.
+    /// 
+    /// # Formula
+    /// The normalized vector `a` is calculated as:
+    /// `a / |a|`.
+    pub fn normalize(&self) -> Vec3 {
+        self.div(self.len())
+    }
+
+    /// Calculates the cross product of two vectors.
+    /// 
+    /// # Arguments
+    /// - `self`: The first vector.
+    /// - `other`: The second vector.
+    /// 
+    /// # Returns
+    /// The cross product as a `Vec3` value.
+    /// 
+    /// # Formula
+    /// The cross product of two vectors `a` and `b` is calculated as:
+    /// `a x b = (a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x)`.
+    pub fn cross(&self, other: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
     }
 
     /// Rotates the vector around the X-axis by a given angle.
